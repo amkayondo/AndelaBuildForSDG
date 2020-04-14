8a8a1ad4ac23fb97053415e332a61d7b6327b43e
amkayondo <amkayondo01@gmail.com>
2020-04-15 00:37:25 +0300

    APP: UI - modify UI - add BG image - add GitHub Repo Link

--- src/components/UserInputForm.js
+++ src/components/UserInputForm.js
@@ -3,23 +3,32 @@
 import React, { Component, lazy, Suspense } from "react";
 import LoadingForm from "./LoadingForm";
 import "../assets/css/formStyle.css";
+import BgImg from "../assets/images/covid-bg.png";
 
 const InputData = lazy(() => import("./InputData"));
 
 export default class UserInputForm extends Component {
   render() {
     return (
+      <div>
+        <div className="tpdf">
+          <a href="https://bit.ly/2VutZmr">
+            See on GitHub
+          </a>
+        </div>
       <div class="mainfds">
       <div class="div-form">
         <div class="div-list">
           <div class="div-item">
-            <div class="div-item_sl"></div>
+            <div class="div-item_sl">
+              <img className="bgimgd" src={BgImg} />
+            </div>
           </div>
         </div>
         <Suspense fallback={<LoadingForm/>}>
           <InputData/>
         </Suspense>
-        </div></div>
+        </div></div></div>
     );
   }
 }
