333d37332bd4349b5789dbd551e0d1d36d251938
amkayondo <amkayondo01@gmail.com>
2020-04-15 15:52:40 +0300

    APP: FIX UI BUILD ERRORS - add nav - add main tag - remove image - add a backgroung image to div-item_sl

--- src/components/UserInputForm.js
+++ src/components/UserInputForm.js
@@ -11,26 +11,26 @@ export default class UserInputForm extends Component {
   render() {
     return (
       <div>
-        <div className="tpdf">
+        <nav className="tpdf">
           <a href="https://bit.ly/2VutZmr">
             See on GitHub
           </a>
-        </div>
-      <div class="mainfds">
+        </nav>
+      <main class="mainfds">
       <div class="div-form">
         <div class="div-list">
           <div class="div-item">
-            <div class="div-item_sl">
-              <img className="bgimgd" alt="
-              Image showing covid-19 estimatation
-              " src={BgImg} />
+            <div class="div-item_sl"
+            style={{ background: `url(${BgImg}) center center / cover` }}
+            >
             </div>
           </div>
         </div>
         <Suspense fallback={<LoadingForm/>}>
           <InputData/>
         </Suspense>
-        </div></div></div>
+        </div></main>
+        </div>
     );
   }
 }
